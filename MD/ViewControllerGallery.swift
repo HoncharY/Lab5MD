797062b321d1e4b78be296c5da59985be6bdd21c
HoncharY <56404915+HoncharY@users.noreply.github.com>
2021-01-04 15:48:55 +0200

    Add files via upload

---
+++ MD/ViewControllerGallery.swift
@@ -0,0 +1,65 @@
+//
+//  VievControllerGallery.swift
+//  MobDev
+//
+//  Created by Honchar on 04.01.2021.
+//
+
+import UIKit
+
+@available(iOS 13.0, *)
+class ViewControllerGallery: UITableViewController, UICollectionViewDataSource {
+    
+    private let collectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: ViewControllerGallery.createLayout()
+    )
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(collectionView)
+        collectionView.register(MyCollectionViewCell.self, forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+        collectionView.frame = view.bounds
+        collectionView.backgroundColor = .lightGray
+        collectionView.dataSource = self
+    }
+    
+    static func createLayout() -> UICollectionViewCompositionalLayout {
+        // Item
+        
+        let item = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(3/4), heightDimension: .fractionalHeight(1)))
+        item.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        let verticalStackItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)))
+        verticalStackItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        let VerticalStackGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1/4), heightDimension: .fractionalHeight(1.0)), subitem: verticalStackItem, count: 3)
+        
+        let quadroItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1/4)))
+        quadroItem.contentInsets = NSDirectionalEdgeInsets(top: 2, leading: 2, bottom: 2, trailing: 2)
+        
+        let quadroHorizontalGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(1/4)), subitem: quadroItem, count: 4)
+        
+        // Group
+        let horizontalGroup = NSCollectionLayoutGroup.horizontal(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(3/4)),subitems: [VerticalStackGroup, item])
+        
+        let verticalGroup = NSCollectionLayoutGroup.vertical(layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1)),subitems: [horizontalGroup, quadroHorizontalGroup])
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: verticalGroup)
+        
+        // Return
+        return UICollectionViewCompositionalLayout(section: section)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // Количество картинок
+        return 14
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath)
+        return cell
+    }
+}
